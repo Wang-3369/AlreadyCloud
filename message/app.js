f175b1a3584a3a6e1c9547fd174daa468bfc4eac
Wang-3369 <s0970603655@gmail.com>
2024-12-21 07:37:51 +0800

    databaseonline

--- message/app.js
+++ message/app.js
@@ -2,32 +2,39 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const { handleSearch } = require('./routes/search');
-const { handleUploadCourse, replaceCourse } = require('./routes/uploadCourse'); // 確保引入處理上傳課程的函數
+const { handleUploadCourse, replaceCourse } = require('./routes/uploadCourse');
 const { handleLogin, handleRegister } = require('./routes/auth');
 const { maplink } = require('./routes/mapsearch');
 const getCoursesRoute = require('./routes/getCourses');
 const detailRoute = require('./routes/detail');
-const { MongoClient } = require('mongodb');
 const cookieParser = require('cookie-parser');
 const favicon = require('serve-favicon');
 const app = express();
 const port = 3000;
 
-const uri = 'mongodb://localhost:27017/school';
-const client = new MongoClient(uri);
-const dbName = 'school';
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://s0970603655:sdClglqFKkKJVJhd@cluster0.rh8bh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// 连接 MongoDB 并返回数据库对象
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 async function connectToMongo() {
     try {
         await client.connect();
         console.log('已連接到 MongoDB');
-        return client.db(dbName);  // 返回数据库对象
+        return client.db('school');  // 返回資料庫物件
     } catch (err) {
         console.error('連接到 MongoDB 出錯:', err);
         throw err;
     }
 }
+
 const cors = require('cors');
 app.use(cors());  // 開啟 CORS 支援
 
@@ -40,13 +47,11 @@ connectToMongo().then((db) => {
     app.use(bodyParser.json());
     app.use(express.static('public')); // 提供靜態檔案服務
 
+    // 設定路由
     app.use('/api', getCoursesRoute(db));
-    // 路由設置
-    //app.post('/')
     app.post('/login', (req, res) => handleLogin(req, res, db));
     app.post('/register', (req, res) => handleRegister(req, res, db));
     app.post('/search', handleSearch);
-    app.post('/uploadCourse', (req, res) => replaceCourse(req, res, db));
     app.post('/uploadCourse', (req, res) => handleUploadCourse(req, res, db));   // 綁定上傳課程處理函數
     app.post('/mapsearch', (req, res) => maplink(req, res, db));  // 传递 db 到 maplink 函数
 
